8c5fdf81ca2ed5f7151a63c3b5ac127ddaf0c18b
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-04-26 19:16:59 +0530

    #include<stdio.h> int main() {     int l,r,k,c=0,i,n;     scanf("%d%d%d",&l,&r,&k);     for(i=l;i<=r;i++)     {         if(i%k==0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_Divisors.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    int l,r,k,c=0,i,n;
+    scanf("%d%d%d",&l,&r,&k);
+    for(i=l;i<=r;i++)
+    {
+        if(i%k==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
+    
+
