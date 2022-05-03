658404cd0201903505922b641a9dd745af64a106
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-05-03 10:12:30 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=0,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j] && i!=j)             {                 c++;             }         }         if(c==0)         {             printf("%d ",arr[i]);             s++;         }     }     if(s==0)     {         printf("-1");     } }

---
+++ Display_unique_values_in_an_Array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=0,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j] && i!=j)
+            {
+                c++;
+            }
+        }
+        if(c==0)
+        {
+            printf("%d ",arr[i]);
+            s++;
+        }
+    }
+    if(s==0)
+    {
+        printf("-1");
+    }
+}
