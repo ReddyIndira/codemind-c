26a8cee25b1609e6a7a5ee7352ded97970b3253e
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-06-04 11:41:40 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,s,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=1;j<arr[i];j++)         {             if(arr[i]%j==0)             {                 c++;             }         }         if(c==1)         {             s++;         }     }     printf("%d",s);
    }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,s,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=1;j<arr[i];j++)
+        {
+            if(arr[i]%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            s++;
+        }
+    }
+    printf("%d",s);
+    
+}
