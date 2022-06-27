eb0e8a97f51f26bdede80539ca8121c59aa74a0d
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-06-27 21:06:14 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],s=0,c,avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(avg<=arr[i])         {             c++;         }     }     printf("%d",c); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],s=0,c,avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+arr[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(avg<=arr[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
+    
