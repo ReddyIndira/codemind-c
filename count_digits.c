77b1c10c1b20752c925a3aab9ef44cea7571aba3
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-06-28 19:19:47 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c,d,t;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<=n-1;i++)     {         c=0;         t=a[i];         while(t!=0)         {             c++;             d=t%10;             t=t/10;         }         if(a[i]==0)         {             c=1;         }         printf("%d ",c);     } }

--- count_digits.c
+++ count_digits.c
@@ -1,26 +1,50 @@
+
+
+
+
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],c=0,t,i,d;
+    int n,i,a[100],c,d,t;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
     for(i=0;i<=n-1;i++)
     {
         c=0;
-        t=arr[i];
+        t=a[i];
         while(t!=0)
         {
             c++;
             d=t%10;
             t=t/10;
         }
-        if(arr[i]==0)
+        if(a[i]==0)
         {
             c=1;
         }
         printf("%d ",c);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
