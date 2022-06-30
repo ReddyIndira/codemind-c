948e1a4d856eecfbde799fddf312b54a79a5f45d
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-06-30 21:21:13 +0530

    #include<stdio.h> int main() {     int i,n,a[100],c=0,s=0,j,m;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&m);     for(i=0;i<n;i++)     {         c=0;         for(j=1;j<=a[i];j++)         {             if(a[i]%j==0)             c++;         }         if(c==2 && a[i]<=m)         {            s++;         }     }     printf("%d",s); }

--- primes_less_than_k.c
+++ primes_less_than_k.c
@@ -1,28 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],s,k,i,j,b;
+    int i,n,a[100],c=0,s=0,j,m;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
-    scanf("%d",&b);
-    for(i=0;i<=n;i++)
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
     {
-        s=0;
-        if(arr[i]<=b)
-        for(j=1;j<arr[i];j++)
+        c=0;
+        for(j=1;j<=a[i];j++)
         {
-            if(arr[i]%j==0)
-            {
-                s++;
-            }
+            if(a[i]%j==0)
+            c++;
         }
-        if(s==1)
+        if(c==2 && a[i]<=m)
         {
-            k++;
+           s++;
         }
     }
-    printf("%d",k);
+    printf("%d",s);
 }
