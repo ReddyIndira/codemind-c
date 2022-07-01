0d6567fb98e30deae4c6b921a3f7a86456a62043
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-07-01 07:36:23 +0530

    #include<stdio.h> int main() {     int n,m,i,p,c,k,j,a[100];     scanf("%d",&p);     k=0;     for(i=1;i<p;i++)     {         if(p%i==0)         {             c=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 {                     c++;                 }             }             if(c==2)             {                 a[k]=i;                 k++;             }         }     }     int y=0;     for(i=0;i<k;i++)     {         for(j=1;j<=k;j++)         {             if(a[i]*a[j]==p)             {                 printf("%d %d",a[i],a[j]);                 y++;                 break;
                }
            }
        }
        if(y==0)
        {
            printf("-1");
        }
    }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,34 +1,43 @@
 #include<stdio.h>
 int main()
 {
-    int n,count=0,k=0,temp,i,a[100],j,b[100],c=0;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {   count=0;
-        for(j=1;j<=i;j++)
-        {
-            if(i%j==0)
-            count++;
-        }
-        if(count==2)
-        {
-        b[k]=i;
-        k++;
-        }
-    }
-    for(i=0;i<k;i++)
+    int n,m,i,p,c,k,j,a[100];
+    scanf("%d",&p);
+    k=0;
+    for(i=1;i<p;i++)
     {
-        for(j=i+1;j<k;j++)
+        if(p%i==0)
         {
-            if(b[i]*b[j]==n)
+            c=0;
+            for(j=1;j<=i;j++)
             {
-            printf("%d %d",b[i],b[j]);
-            c++;
-            break;
+                if(i%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                a[k]=i;
+                k++;
             }
         }
     }
-    if(c==0)
+    int y=0;
+    for(i=0;i<k;i++)
+    {
+        for(j=1;j<=k;j++)
+        {
+            if(a[i]*a[j]==p)
+            {
+                printf("%d %d",a[i],a[j]);
+                y++;
+                break;
+                
+            }
+        }
+    }
+    if(y==0)
     {
         printf("-1");
     }
