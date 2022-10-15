22186fba54d1bdcdb9d0c925ac58e960e4a845de
ReddyIndira <98727534+ReddyIndira@users.noreply.github.com>
2022-10-15 22:48:47 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     char a[100],b[100];     scanf("%s%s",a,b);     long long int k=atoi(a);     long long int l=atoi(b);     printf("%lld",l*k); }

---
+++ Multiply_Strings.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char a[100],b[100];
+    scanf("%s%s",a,b);
+    long long int k=atoi(a);
+    long long int l=atoi(b);
+    printf("%lld",l*k);
+}
+    
+    
